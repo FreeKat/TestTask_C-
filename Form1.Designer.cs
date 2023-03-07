c2a69c5b352d88c65201adfe099ffd808289cd25
FreeKat <126882963+FreeKat@users.noreply.github.com>
2023-03-07 15:15:30 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace TaskManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtReplica = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Replica:";
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(138, 84);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(527, 22);
+            this.txtSource.TabIndex = 2;
+            // 
+            // txtReplica
+            // 
+            this.txtReplica.Location = new System.Drawing.Point(138, 141);
+            this.txtReplica.Name = "txtReplica";
+            this.txtReplica.Size = new System.Drawing.Size(527, 22);
+            this.txtReplica.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(690, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = " ... ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(690, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = " ... ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(347, 186);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 54);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Syncronize";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(12, 246);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(785, 148);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.button3_Click);
+            this.listBox1.SelectedValueChanged += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(561, 400);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(236, 41);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Save a full Report.txt file";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 443);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtReplica);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Syncronize Folders";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtReplica;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace TaskManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog open = new FolderBrowserDialog();
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                txtSource.Text = open.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog target = new FolderBrowserDialog();
+            if (target.ShowDialog() == DialogResult.OK)
+            {
+                txtReplica.Text = target.SelectedPath;
+            }
+        }
+        
+        static public void CopyDirectory(string SourceDirectory, string ReplicaDirectory)
+        {
+            
+            DirectoryInfo source = new DirectoryInfo(SourceDirectory);
+            DirectoryInfo replica = new DirectoryInfo(ReplicaDirectory); 
+
+            //Copy files.
+            FileInfo[] sourceFiles = source.GetFiles();
+            for (int i = 0; i < sourceFiles.Length; ++i)
+                File.Copy(sourceFiles[i].FullName, replica.FullName + "\\" + sourceFiles[i].Name, true);
+
+            //Copy directories.
+            DirectoryInfo[] sourceDirectories = source.GetDirectories();
+            for (int j = 0; j < sourceDirectories.Length; ++j)
+                CopyDirectory(sourceDirectories[j].FullName, replica.FullName + "\\" + sourceDirectories[j].Name);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (txtSource.Text == string.Empty)
+            {
+                MessageBox.Show("Source File does not exist!", "Error");
+                return;
+            }
+            
+            DirectoryInfo source = new DirectoryInfo(txtSource.Text);
+            DirectoryInfo replica = new DirectoryInfo(txtReplica.Text);
+
+            //Determine whether the source directory exists.
+            if (!source.Exists)
+                return;
+            //Determine whether the replica directory exists.
+            if (!replica.Exists)
+                replica.Create();
+
+
+            replica.Delete(true);
+            replica.Create();
+
+            FileInfo[] sourceFiles = source.GetFiles();
+            for (int i = 0; i < sourceFiles.Length; ++i)
+                File.Copy(sourceFiles[i].FullName, replica.FullName + "\\" + sourceFiles[i].Name, true);
+
+            //Copy directories.
+            DirectoryInfo[] sourceDirectories = source.GetDirectories();
+            for (int j = 0; j < sourceDirectories.Length; ++j)
+                CopyDirectory(sourceDirectories[j].FullName, replica.FullName + "\\" + sourceDirectories[j].Name);
+
+            MessageBox.Show("Replica Folder has been updated successfully", "Job Finished!");
+
+            WriteToProgress(" #Replica Folder has been updated successfully \n");
+            WriteToProgress(" Detailed report \n");
+            for (int i = 0; i < sourceFiles.Length; ++i)
+            WriteToProgress(sourceFiles[i].FullName + " copied successfully.");            
+            var progress = WriteToProgress("\n \n End of report.");
+
+        }
+
+        public List<string> WriteToProgress(string step)
+        {
+            if (!string.IsNullOrEmpty(step))
+                listBox1.Items.Add(step);
+            return listBox1.Items.OfType<Object>().Where(o => o != null).Select(o => o.ToString()).ToList();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var saveFile = new SaveFileDialog();
+            saveFile.Filter = "Text (*.txt)|*.txt";
+            saveFile.FileName = "Raport";
+            if (saveFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                using (var sw = new System.IO.StreamWriter(saveFile.FileName, false))
+                    foreach (var item in listBox1.Items)
+                        sw.Write(item.ToString() + Environment.NewLine); // Environment.CurrentDirectory);
+                MessageBox.Show("The file has been saved successfully");
+            }
+        }
+
+        
+    }
+}
